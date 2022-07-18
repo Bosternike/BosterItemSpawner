43ed7bf772fc5bd7ccb1040dce38869187b45178
Bosternike <aav0918@gmail.com>
2022-07-18 11:54:01 +0200

    t

--- plugin/src/main/java/net/Boster/item/spawner/holo/Hologram.java
+++ plugin/src/main/java/net/Boster/item/spawner/holo/Hologram.java
@@ -10,7 +10,6 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
